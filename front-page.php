27790df26f209829ef0cfa0fa710b12178e43557
Przemek Szulecki <p.szulecki@gmail.com>
2026-04-21 10:27:19 +0200

    Add security section to homepage

--- front-page.php
+++ front-page.php
@@ -5,6 +5,7 @@
 <?php get_template_part('template-parts/sports'); ?>
 <?php get_template_part('template-parts/shootero'); ?>
 <?php get_template_part('template-parts/why'); ?>
+<?php get_template_part('template-parts/security'); ?>
 <?php get_template_part('template-parts/steps'); ?>
 <?php get_template_part('template-parts/pricing'); ?>
 <?php get_template_part('template-parts/faq'); ?>
