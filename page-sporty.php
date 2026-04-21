ab2e1f209af98ace594bdbb71dc02702fd46cb69
Przemek Szulecki <p.szulecki@gmail.com>
2026-04-21 12:09:31 +0200

    page-sporty: include extra sports template (parts 1+2)

--- page-sporty.php
+++ page-sporty.php
@@ -1,7 +1,6 @@
-<?php
-/*
-Template Name: Sporty
-*/
+<?php /* Template Name: Sporty */
 get_header();
 get_template_part('template-parts/page-sporty-content');
+get_template_part('template-parts/page-sporty-extra');
 get_footer();
+?>
